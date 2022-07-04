191d86e54e88fbed574d3a127752661905ddcd4f
javi-diaz47 <javiereduardo300@gmail.com>
2022-07-03 20:14:22 -0500

    chore: add eslint configuration

---
+++ components/Card/index.jsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/components/Card/index.module.css';
+
+function Card() {
+  return (
+    <div className={styles.card}>
+      <h2>This is a Card</h2>
+    </div>
+  );
+}
+
+export { Card };
