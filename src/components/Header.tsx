7dd475f50e6eb8e32414d2673e3f15a9705d7f17
javi-diaz47 <javiereduardo300@gmail.com>
2024-07-11 18:47:05 -0500

    ✨ [ADD] toggle navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
 import { MenuIcon } from "../icons/MenuIcon";
 import { XIcon } from "../icons/XIcon";
+import { twMerge } from "tailwind-merge";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+  const toggleNavbar = () => {
+    console.log(isOpen);
+    const newOpen = !isOpen;
+    setIsOpen(newOpen);
+  };
+
   return (
-    <header className="flex justify-between px-4 max-w-sm">
+    <header className="flex justify-between px-4 max-w-2xl bg-white py-2 shadow-neutral-200 shadow-lg">
       <p className="font-extrabold">
         Pomo<span className="text-red-400">Timer</span>
       </p>
-      <button className="">
-        <MenuIcon className="size-10" />
-        <XIcon className="size-10"/>
+      <button
+        onClick={toggleNavbar}
+        className="relative size-12 rounded-xl grid place-items-center z-10"
+      >
+        <MenuIcon
+          className={twMerge(isOpen && "sr-only", "size-10 absolute")}
+        />
+        <XIcon className={twMerge(!isOpen && "sr-only", "size-10 absolute text-white")} />
       </button>
-      <nav className="fixed top-0 -right-full w-full h-full bg-primary transition-all ease-in-out">
+      <nav
+        className={twMerge(
+          isOpen && "-right-0",
+          !isOpen && "-right-full",
+          "fixed top-0 w-full h-full bg-primary transition-all ease-in-out",
+        )}
+      >
         <ul className="text-white font-bold w-full h-full flex flex-col justify-center items-center gap-12">
           <li>
             <a href="/" className="line-through">
--- src/icons/XIcon.tsx
+++ src/icons/XIcon.tsx
@@ -7,7 +7,7 @@ export function XIcon({ className }: Icon) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth="1.5"
+      strokeWidth="2.5"
       stroke="currentColor"
       className={twMerge("size-6", className)}
     >
