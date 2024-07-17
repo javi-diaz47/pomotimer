2754d1bbf4813fd6456172aef0a01c560e529540
javi-diaz47 <javiereduardo300@gmail.com>
2024-07-17 17:22:47 -0500

    💄 [ADD] add styles to timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,3 @@
-import { DEFAULT_TIME } from '../constants'
 import { type Timer } from '../hooks/useTimer'
 
 interface TimerProps extends Timer {
@@ -17,29 +16,37 @@ export function Timer({
   total,
 }: TimerProps) {
   return (
-    <div>
-      <h2 id="timer" className="text-6xl">
-        {time}
-      </h2>
-      <div className="text-4xl">
-        <p>{title}</p>
-        <span className="text-2xl">
-          {completed}/{total}
-        </span>
+    <section className={`flex flex-col items-center ${isActive ? 'gap-32' : 'gap-8'}`}>
+
+      <div className='w-64 aspect-square bg-white rounded-full flex justify-center items-center relative shadow-[4px_4px_30px_0_#d86971]' >
+        <div className=''>
+          <h2 id="timer" className="text-7xl text-center">
+            {time}
+          </h2>
+          <div className=" grid place-items-center absolute left-1/2 -translate-x-1/2">
+            <p className='text-center text-3xl'>
+              {title}
+            </p>
+            <span className="text-lg">
+              {completed}/{total}
+            </span>
+          </div>
+        </div>
       </div>
+
       <div className="flex flex-col">
         <button
           onClick={play}
-          className="w-56 bg-red-300 py-2 px-12 rounded-full font-bold">
+          className="w-56 bg-red-400 py-2 px-12 rounded-full font-bold">
           {!isActive ? 'Start' : 'Pause'}
         </button>
         <button
           onClick={cancel}
-          className={`w-56 bg-white text-red-300 py-2 px-12 rounded-full
+          className={`w-56 bg-white text-red-400 py-2 px-12 rounded-full
           transition-transform ease-out ${isActive ? 'scale-1' : 'scale-0 '}`}>
           Cancel
         </button>
       </div>
-    </div>
+    </section >
   )
 }
