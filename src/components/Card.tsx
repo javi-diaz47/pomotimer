f9d8faf7b4355515bf78c261f088454f03e9dd58
javi-diaz47 <javiereduardo300@gmail.com>
2024-07-19 15:35:18 -0500

    💄 [ADD] add styles and center cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,12 +8,14 @@ namespace Card {
 
 export function Card({ icon, title, times }: Card.Props) {
   return (
-    <div className="flex justify-around items-center bg-white rounded-2xl shadow-md py-3">
-      <div className="w-fit aspect-square rounded-full bg-red-300 p-2">
-        <span>{icon}</span>
+    <div className="w-full px-4 flex justify-center">
+      <div className="w-full max-w-lg flex justify-around items-center gap-4 py-2 text-2xl rounded-2xl shadow-md">
+        <div className="w-fit aspect-square rounded-full bg-red-300 p-2">
+          <span>{icon}</span>
+        </div>
+        <span className="capitalize text-primary font-extrabold text-3xl">{title}</span>
+        <span className="text-red-300 font-bold">{times[0]}/{times[1]}</span>
       </div>
-      <span className="capitalize text-primary font-extrabold">{title}</span>
-      <span className="text-red-300 font-bold">{times[0]}/{times[1]}</span>
     </div>
   )
 }
